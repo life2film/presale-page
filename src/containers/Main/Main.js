8aa444ea9760fc71c9e0c0572e98458d648bda29
Andrey Bondar <a.y.bondar@gmail.com>
2017-03-19 23:47:50 +0300

    feat(socials): add component

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,6 +1,8 @@
 import React, {PureComponent} from 'react';
 import Title from '../../components/Title';
 import Faq from '../../components/Faq';
+import Ask from '../../components/Ask';
+
 
 import './Main.css';
 
@@ -8,9 +10,14 @@ export default class Main extends PureComponent {
     render() {
         return (
             <main className="main">
-                <Title size="big">SONM tokens presale</Title>
+                <div className="main__title">
+                    <Title size="big">SONM tokens presale</Title>
+                </div>
                 <div className="main__faq">
                     <Faq />
+                </div>
+                <div className="main__ask">
+                    <Ask />
                 </div>
             </main>
         )
---
+++ src/data/socials.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        icon: 'reddit.png',
+        text: 'Reddit',
+        url: 'javascript:void(0)'
+    },
+    {
+        icon: 'bitcoin.png',
+        text: 'BitcoinTalk',
+        url: 'javascript:void(0)'
+    },
+    {
+        icon: 'github.png',
+        text: 'GitHub',
+        url: 'javascript:void(0)'
+    },
+    {
+        icon: 'slack.png',
+        text: 'Slack',
+        url: 'javascript:void(0)'
+    },
+    {
+        icon: 'twitter.png',
+        text: 'Twitter',
+        url: 'javascript:void(0)'
+    },
+    {
+        icon: 'facebook.png',
+        text: 'Facebook',
+        url: 'javascript:void(0)'
+    },
+    {
+        icon: 'google.png',
+        text: 'Google Groups',
+        url: 'javascript:void(0)'
+    },
+    {
+        icon: 'telegram.png',
+        text: 'Telegram',
+        url: 'javascript:void(0)'
+    },
+    {
+        icon: 'medium.png',
+        text: 'Medium',
+        url: 'javascript:void(0)'
+    }
+];

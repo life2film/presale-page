b18da3f0be5a2fda5162f11da8ebfa75325fd7b6
Andrey Bondar <a.y.bondar@gmail.com>
2017-03-19 17:31:11 +0300

    feat(*): add template, header

---
+++ src/components/Page/Page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../Header';
+import './Page.css';
+
+const Page = () => {
+    return (
+        <div className="page">
+            <div className="page__header">
+                <Header />
+            </div>
+        </div>
+    )
+};
+
+export default Page; 

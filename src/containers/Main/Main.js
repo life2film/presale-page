a6a1de2b1fad8bd86532944f00546d8738de0649
Andrey Bondar <a.y.bondar@gmail.com>
2017-03-19 21:54:00 +0300

    feat(main): add faq block

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -1,4 +1,6 @@
 import React, {PureComponent} from 'react';
+import Title from '../../components/Title';
+import Faq from '../../components/Faq';
 
 import './Main.css';
 
@@ -6,7 +8,10 @@ export default class Main extends PureComponent {
     render() {
         return (
             <main className="main">
-                Main
+                <Title size="big">SONM tokens presale</Title>
+                <div className="main__faq">
+                    <Faq />
+                </div>
             </main>
         )
     }
---
+++ src/data/faq.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        text: 'What are the benefits of early investing to SONM on presale?',
+        popup: 'No centralized authority, regulating computing resource distribution. We are also support tools for anonymity like proxy or TOR. '
+    },
+    {
+        text: 'What are the benefits of early investing to SONM on presale?',
+        popup: 'No centralized authority, regulating computing resource distribution. We are also support tools for anonymity like proxy or TOR.'
+    },
+    {
+        text: 'What are the benefits of early investing to SONM on presale?',
+        popup: 'No centralized authority, regulating computing resource distribution. We are also support tools for anonymity like proxy or TOR.'
+    },
+    {
+        text: 'What are the benefits of early investing to SONM on presale?',
+        popup: 'No centralized authority, regulating computing resource distribution. We are also support tools for anonymity like proxy or TOR.'
+    },
+    {
+        text: 'What are the benefits of early investing to SONM on presale?',
+        popup: 'No centralized authority, regulating computing resource distribution. We are also support tools for anonymity like proxy or TOR.'
+    },
+    {
+        text: 'What are the benefits of early investing to SONM on presale?',
+        popup: 'No centralized authority, regulating computing resource distribution. We are also support tools for anonymity like proxy or TOR.'
+    }
+]
